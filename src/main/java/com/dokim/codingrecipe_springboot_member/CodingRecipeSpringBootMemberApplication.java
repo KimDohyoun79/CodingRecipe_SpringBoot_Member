05bd4bce372ee652e6b9db594388712bf3ec3521
KimDohyoun79 <dogdo5726@gmail.com>
2022-12-14 15:40:31 +0900

    [ADD] first commit

---
+++ src/main/java/com/dokim/codingrecipe_springboot_member/CodingRecipeSpringBootMemberApplication.java
@@ -0,0 +1,13 @@
+package com.dokim.codingrecipe_springboot_member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingRecipeSpringBootMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingRecipeSpringBootMemberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dokim/codingrecipe_springboot_member/CodingRecipeSpringBootMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dokim.codingrecipe_springboot_member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingRecipeSpringBootMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
